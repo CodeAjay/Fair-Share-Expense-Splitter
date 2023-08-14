2707cbdfb43e65e745de2668dac0228819e12cf7
CodeAjay <ajay01937@gmail.com>
2023-08-14 16:00:57 +0530

    Style changes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,12 +7,25 @@
 // export default Footer;
 
 import React from "react";
+import { Row, Col,Container } from "react-bootstrap";
+import { FaInstagram,FaGithub,FaYoutube } from "react-icons/fa";
+import "../styles.css";
 
 const Footer = () => {
   const githubUrl = "https://github.com/codeajay";
 
   return (
     <footer style={footerStyle}>
+      <Container>
+        <Row className="ftrics">
+          <Col md={6}>
+            <a href="https://instagram.com/devlab01" target="_blank"><FaInstagram /></a>
+            <a href="https://github.com/codeajay" target="_blank"><FaGithub /></a>
+            <a href="https://youtube.com/@devlab01" target="_blank"><FaYoutube /></a>
+          </Col>
+          
+        </Row>
+      </Container>
       <p>
         &copy; {new Date().getFullYear()} FairShare. All rights reserved.{" "}
         <a href={githubUrl} target="_blank" rel="noopener noreferrer">
